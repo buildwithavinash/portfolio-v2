c94e228fe6eed73c801129f617c0cdeaf3e52a78
Avinash <buildwithavinash@users.noreply.github.com>
2026-04-12 23:18:21 +0530

    update: about polish

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -4,22 +4,22 @@ const About = () => {
   return (
     <section className="py-16">
         <Container>
-            <div className="max-w-2xl">
+            <div className="max-w-xl mx-auto">
                 <div>
-                <h2>About ME</h2>
+                <h2 className="text-3xl md:text-5xl text-center font-semibold text-zinc-800">About Me</h2>
                 </div>
 
-                <div className="space-y-2">
-                    <p className="leading-relaxed">I’m a self-taught frontend developer focused on building clean,
+                <div className="space-y-4 mt-8">
+                    <p className="leading-relaxed text-zinc-800">I’m a self-taught frontend developer focused on building clean,
             practical web applications.</p>
 
-                    <p>
+                    <p className="leading-relaxed text-zinc-800">
                         I enjoy working with React and JavaScript, and I care about writing
             code that is readable, maintainable, and actually useful to the user.
                     </p>
 
-                    <p>
-                        Currently, I’m looking for opportunities to grow, collaborate, and
+                    <p className="leading-relaxed text-zinc-800">
+                        Currently, I’m looking for <mark className="bg-green-400">opportunities</mark> to grow, collaborate, and
             contribute to real-world projects.
                     </p>
                 </div>
